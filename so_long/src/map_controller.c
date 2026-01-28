fe5b6c7600fad211f47b2400a0fc2a4522b073e7
Wirayabovorn007 <wirayabovorn606@gmail.com>
2026-01-28 14:33:42 +0700

    Update So_long map validation

--- so_long/src/map_controller.c
+++ so_long/src/map_controller.c
@@ -24,27 +24,11 @@ char	**map_err(char *type)
 	write(2,"Error\n", 6);
 	if (type)
 	{
-		write(1, "Type: ", 2);
+		write(2, "Type: ", 6);
 		write(2, type, len(type));
 	}
 	write(2, "\n", 1);
 	return NULL;
-}
-
-int	validmap_cond3(char *line)
-{
-	int	f;
-	int	l;
-	int	i;
-
-	i = -1;
-	if (line[0] == '1')
-		f = 1;
-	while (line[++i] != '\0')
-		;
-	if (line[i - 1] == '1')
-		l = 1;
-	return (f && l);
 }
 
 int	validmap_cond1(char *line)
@@ -60,39 +44,84 @@ int	validmap_cond1(char *line)
 	}
 	return (1);
 }
+
+int	validmap_cond3(char **map)
+{
+	int	row;
+	int	col;
+	int	st;
+	int	ei;
+	int	collect;
+
+	row = -1;
+	st = 0;
+	ei = 0;
+	collect = 0;
+	while (map[++row])
+	{
+		col = -1;
+		while (map[row][++col])
+		{
+			if (map[row][col] == 'P')
+				st++;
+			if (map[row][col] == 'E')
+				ei++;
+			if (map[row][col] == 'C')
+				collect++;
+		}
+	}
+	return ((st == 1) && (ei == 1) && (collect >= 1));
+}
+
+int	validmap_cond4(char *line)
+{
+	int	f;
+	int	l;
+	int	i;
+
+	i = -1;
+	f = 0;
+	l = 0;
+	if (line[0] == '1')
+		f = 1;
+	while (line[++i] != '\0')
+		;
+	if ((i > 0) && (line[i - 1] == '1'))
+		l = 1;
+	return (f && l);
+}
+
 char **validate_map(char **arr, int line_count)
 {
 	char	**t;
 	int		row;
-	int		col;
 	int		len_fline;
 
 	t = arr;
+	if (line_count < 1 || !t || !t[0])
+		map_err("Map is empty.");
 	row = 0;
-	col = 0;
-	len_fline = len(arr[row]);
-	if ((validmap_cond1(arr[0]) != 1) || (validmap_cond1(arr[line_count - 1]) != 1)) // the first and last line must include only wall (1)
-		return map_err("Invalid line format.");
-	while (arr[row])
+	len_fline = len(t[row]);
+	if ((validmap_cond1(t[0]) != 1) || // the first line must include only wall (1)
+			(validmap_cond1(t[line_count - 1]) != 1) // the last line must include only wall (1)
+				|| (validmap_cond3(t) != 1)) // map must contain 1 == exit, 1 == starting point, at least 1 >= collectible
+		return map_err("Invalid border or components.");
+	while (t[row])
 	{
-		if (len(arr[row]) != len_fline) // all lines must have the same length
+		if (len(t[row]) != len_fline) // all lines must have the same length
 			return (map_err("Invalid line length."));
-		if (validmap_cond3(arr[row]) != 1) // first and last char of every line must be 1
-			return (map_err("Invalid line format."));
-		while (arr[col])
-		{
-			col++;
-		}
+		if (validmap_cond4(t[row]) != 1) // first and last char of every line must be 1
+			return (map_err("Invalid side borders."));
 		row++;
 	}
-
-	// map must contain 1 == exit, 1 == starting point, at least 1 >= collectible
+	return (arr);
 }
 
 char	**read_map(char *path)
 {
 	int		fd;
 	int		i;
+	int		line_len;
 	int		line_count;
 	char	*line;
 	char	**line_arr;
@@ -100,13 +129,18 @@ char	**read_map(char *path)
 	fd = open(path, O_RDONLY);
 	line_count = get_line_count(path);
 	if (fd < 0 || line_count < 0)
-		return (map_err("Opening file."));
+		return (map_err("Empty file or open/read error."));
 	i = 0;
 	line_arr = malloc(sizeof(char *) * (line_count + 1));
 	if (!line_arr)
 		return (map_err("Memory allocation."));
 	while ((line = get_next_line(fd)) != NULL)
+	{
+		line_len = len(line);
+		if (line_len > 0 && line[line_len - 1] == '\n')
+			line[line_len - 1] = '\0';
 		line_arr[i++] = line;
+	}
 	line_arr[i] = NULL;
 	close(fd);
 	return (validate_map(line_arr, line_count));
